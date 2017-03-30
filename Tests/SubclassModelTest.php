f8d25646278daf13a422f4d27d2c0793511ffb93
Acidvertigo <ldanzelmo@gmail.com>
2017-03-30 21:19:23 +0200

    fix test

--- Tests/SubclassModelTest.php
+++ Tests/SubclassModelTest.php
@@ -11,7 +11,7 @@ class MyDomainModel extends BaseDomainModel
     
     public function __construct()
     {
-        parent::__construct(['id']);
+        parent::__construct('id');
         $this->__setFieldNames(self::$fieldNames);
     }
     
